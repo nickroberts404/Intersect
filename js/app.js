00b5edae22b5c64a8da5c2eacf8656b2e0882fd5
nickroberts404 <nickroberts404@gmail.com>
2015-09-27 05:56:51 -0500

    Added login buttons and login controller

--- js/app.js
+++ js/app.js
@@ -1,5 +1,38 @@
 var app = angular.module("hackApp", []);
 
+app.controller("loginController", function($scope){
+	var fire = new Firebase("https://intersecthacktx.firebaseio.com/");
+	console.log($scope);
+	$scope.handleOAuthLogin = function(provider){
+		fire.authWithOAuthPopup(provider, handleLogin);
+	}
+	$scope.handlePasswordLogin = function(){
+		
+		fire.authWithPassword({
+			email: $scope.loginEmail,
+			password: $scope.loginPass
+		}, handleLogin);
+	}
+	function handleLogin(error, userData){
+		if(error){
+			console.log("Error logging in user: ", error);
+		} else{
+			console.log("Succesfully logged in user account: ", userData);
+		}
+	}
+	function handleUserCreation(){
+		fire.createUser({
+			email: loginEmail.val(),
+			password: loginPassword.val()
+		}, function(error, userData){
+			if(error){
+				console.log("Error creating user: ", error);
+			} else{
+				console.log("Succesfully created user account: ", userData);
+			}
+		});
+	}
+});
 // $(document).ready(function(){
 //   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 //   $('.modal-trigger').leanModal();
